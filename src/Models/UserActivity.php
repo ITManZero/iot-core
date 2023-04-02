809510b38871c6855a284d2211351265116c74e8
Bakr Mslmani <baker.mesl.bm@gmail.com>
2023-04-02 11:54:36 +0300

    Fixing

--- src/Models/UserActivity.php
+++ src/Models/UserActivity.php
@@ -8,11 +8,35 @@ use Ite\IotCore\Builders\UserActivityBuilder;
 
 class UserActivity
 {
+    /**
+     * @var int
+     */
     public int $id;
+    /**
+     * Full name
+     * @var string
+     */
     public string $name;
+    /**
+     * Full name
+     * @var string
+     */
     public string $action;
+    /**
+     * Full name
+     * @var string
+     */
     public string $message;
+    /**
+     * Full name
+     * @var bool
+     */
     public bool $isAdmin;
+
+    /**
+     * Full name
+     * @var DateTime
+     */
     public DateTime $expireAt;
 
     public function create(int $id, string $name, string $action, string $message, bool $isAdmin): UserActivity
