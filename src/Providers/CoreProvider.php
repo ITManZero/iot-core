7c155ad5f0d56b4bd00033a76606f1a41d03d660
Bakr Mslmani <baker.mesl.bm@gmail.com>
2023-04-02 12:50:10 +0300

    Refactoring

--- src/Providers/CoreProvider.php
+++ src/Providers/CoreProvider.php
@@ -12,18 +12,24 @@ class CoreProvider extends ServiceProvider
     private array $providers = [
     ];
 
+    private array $commands = [
+        RabbitMQConsumerCommand::class
+    ];
+
     /**
      * Register services.
      */
     public function register()
     {
-        foreach ($this->providers as $provider) {
+        foreach ($this->providers as $provider)
             $this->app->register($provider);
-        }
-        $this->app->singleton(UserActivityContext::class);
-        $this->commands([
-            RabbitMQConsumerCommand::class
-        ]);
+
+        $this->commands($this->commands);
+
+//        $this->app->singleton(UserActivityContext::class);
+        $this->app->singleton(UserActivityContext::class, function () {
+            return new UserActivityContext();
+        });
     }
 
 
