9790fd135a211e4be34ea52cf0b725f789c9e143
Bakr Mslmani <baker.mesl.bm@gmail.com>
2023-04-04 15:29:24 +0300

    Encode Message To String

--- src/Services/MessagingService.php
+++ src/Services/MessagingService.php
@@ -5,6 +5,7 @@ namespace Ite\IotCore\Services;
 use PhpAmqpLib\Channel\AbstractChannel;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class MessagingService
 {
@@ -17,8 +18,10 @@ class MessagingService
         $this->channel = $connection->channel();
     }
 
-    public function publish(string $exchange, string $key, $message): void
+    public function publish(string $exchange, string $key, $data): void
     {
+        $message = new AMQPMessage(json_encode($data));
+
         $this->channel->basic_publish($message, $exchange, $key);
     }
 
