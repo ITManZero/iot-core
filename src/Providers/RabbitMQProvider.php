ef6642cd7c698aeab5170c29cbc85e7580451913
Bakr Mslmani <baker.mesl.bm@gmail.com>
2023-06-10 13:38:25 +0300

    Removing RabbitMQ

--- src/Providers/RabbitMQProvider.php
+++ src/Providers/RabbitMQProvider.php
@@ -9,48 +9,48 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class RabbitMQProvider
 {
-
-    public AMQPStreamConnection $connection;
-    public AbstractChannel|AMQPChannel $channel;
-
-    /**
-     * @throws \Exception
-     */
-    public function __construct()
-    {
-        $this->connection = new AMQPStreamConnection(
-            env('RABBITMQ_HOST'),
-            env('RABBITMQ_PORT'),
-            env('RABBITMQ_USER'),
-            env('RABBITMQ_PASSWORD'),
-            env('RABBITMQ_VHOST')
-        );;
-        $this->channel = $this->connection->channel();
-        $this->init();
-    }
-
-    private function init(): void
-    {
-        $config = config('rabbitmq');
-
-        if ($config) {
-            // creating exchange
-            $exchanges = $config['exchanges'] ?? [];
-            foreach ($exchanges as $exchange) {
-                $this->channel->exchange_declare(
-                    $exchange['name'],
-                    $exchange['type'],
-                    $exchange['passive'],
-                    $exchange['durable'],
-                    $exchange['auto_delete']);
-            }
-
-            // creating and binding queues
-            $queues = $config['queues'] ?? [];
-            foreach ($queues as $queue) {
-                $this->channel->queue_declare($queue['name']);
-                $this->channel->queue_bind($queue['name'], $queue['exchange'], $queue['bind']);
-            }
-        }
-    }
+//
+//    public AMQPStreamConnection $connection;
+//    public AbstractChannel|AMQPChannel $channel;
+//
+//    /**
+//     * @throws \Exception
+//     */
+//    public function __construct()
+//    {
+//        $this->connection = new AMQPStreamConnection(
+//            env('RABBITMQ_HOST'),
+//            env('RABBITMQ_PORT'),
+//            env('RABBITMQ_USER'),
+//            env('RABBITMQ_PASSWORD'),
+//            env('RABBITMQ_VHOST')
+//        );;
+//        $this->channel = $this->connection->channel();
+//        $this->init();
+//    }
+//
+//    private function init(): void
+//    {
+//        $config = config('rabbitmq');
+//
+//        if ($config) {
+//            // creating exchange
+//            $exchanges = $config['exchanges'] ?? [];
+//            foreach ($exchanges as $exchange) {
+//                $this->channel->exchange_declare(
+//                    $exchange['name'],
+//                    $exchange['type'],
+//                    $exchange['passive'],
+//                    $exchange['durable'],
+//                    $exchange['auto_delete']);
+//            }
+//
+//            // creating and binding queues
+//            $queues = $config['queues'] ?? [];
+//            foreach ($queues as $queue) {
+//                $this->channel->queue_declare($queue['name']);
+//                $this->channel->queue_bind($queue['name'], $queue['exchange'], $queue['bind']);
+//            }
+//        }
+//    }
 }
